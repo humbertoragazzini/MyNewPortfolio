dbd1b35b216ef8129130c63e5e4692839557dcf5
humberto ragazzini <beto_vicio@hotmail.com>
2025-05-10 18:11:43 +0200

    working on lightning

--- src/components/organisms/Experience.jsx
+++ src/components/organisms/Experience.jsx
@@ -8,10 +8,19 @@ import Camera from "../atoms/Camera";
 import ProjectLeft from "../atoms/ProjectLeft";
 import IframedLeft from "../atoms/IframedLeft";
 import IframedRight from "../atoms/IframedRight";
+import * as THREE from "three";
 
 export default function Experience() {
   const scrollContainerRef = useRef();
   const [scroll, setScroll] = useState(0);
+  const lightRef = useRef();
+  const targetRef = useRef();
+
+  useEffect(() => {
+    if (lightRef.current && targetRef.current) {
+      lightRef.current.target = targetRef.current;
+    }
+  }, []);
 
   return (
     <div className="w-full h-screen overflow-hidden bg-black">
@@ -48,6 +57,43 @@ export default function Experience() {
                 positionZ={-60}
                 url={"https://humbertoragazzini.github.io/TheSuperGame/"}
               ></IframedLeft>
+              <directionalLight
+                ref={lightRef}
+                position={[0, 25, 0]}
+                intensity={1}
+                castShadow
+                shadow-mapSize-width={2048}
+                shadow-mapSize-height={2048}
+                shadow-camera-left={-25}
+                shadow-camera-right={25}
+                shadow-camera-top={25}
+                shadow-camera-bottom={-25}
+                shadow-camera-near={1}
+                shadow-camera-far={50}
+              />
+              <mesh
+                ref={targetRef}
+                position={[0, 0, 50]}
+                castShadow
+                receiveShadow
+              >
+                <boxGeometry args={[5, 5, 5]}></boxGeometry>
+                <meshStandardMaterial color={"white"}></meshStandardMaterial>
+                {/* <meshBasicMaterial></meshBasicMaterial> */}
+              </mesh>
+              <mesh
+                rotation={[-Math.PI / 2, 0, 0]}
+                position={[0, -6, 50]}
+                castShadow
+                receiveShadow
+              >
+                <planeGeometry args={[15, 15]}></planeGeometry>
+                <meshStandardMaterial
+                  color={"white"}
+                  side={THREE.DoubleSide}
+                ></meshStandardMaterial>
+                {/* <meshBasicMaterial></meshBasicMaterial> */}
+              </mesh>
               <MainScene />
             </Canvas>
           </motion.div>
