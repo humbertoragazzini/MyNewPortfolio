afc9359566dccd77d87aa738c4528b5b44308b9f
humbertoragazzini <beto_vicio@hotmail.com>
2025-05-21 20:05:54 +0000

    building the final screen

--- src/components/atoms/Final.jsx
+++ src/components/atoms/Final.jsx
@@ -3,8 +3,15 @@ import { Html } from "@react-three/drei";
 export default function Final() {
     return (
         <Html occlude={"blending"} transform position={[0, 0, -1450]}>
-            <div className="w-screen h-screen bg-white">
-                <h1>The final content</h1>
+            <div className="w-screen h-screen bg-black">
+                <div className="grid grid-cols-2">
+                    <div className="col-span-1">
+                        <img></img>
+                    </div>
+                    <div className="col-span-1">
+
+                    </div>
+                </div>
             </div>
         </Html>
     )
