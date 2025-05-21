5d6bfcf15decb58dd6a26092ea5a3e42d584d149
humbertoragazzini <beto_vicio@hotmail.com>
2025-05-21 20:28:14 +0000

    adding some small content

--- src/components/atoms/Final.jsx
+++ src/components/atoms/Final.jsx
@@ -10,20 +10,26 @@ export default function Final() {
                     </div>
                     <div className="col-span-1">
                         <ul>
-                            <li>
-                                Gracias por haber visitado mi portfolio, esto abierto a nuevos desafios y a seguir creciendo, si tenes algun projecto en mente, nos pongamos en contacto y hagamos de ese proejcto una realidad.
+                            <li className="text-5xl font-semibold mb-5">
+                                Gracias por haber visitado mi portfolio
                             </li>
-                            <li>
+                            <li className="text-3xl mb-6">
+                                Estoy abierto a nuevos desafios y a seguir creciendo, si tenes algun projecto en mente, nos pongamos en contacto y hagamos de ese proejcto una realidad.
+                            </li>
+                            <li className="text-3xl mb-6">
                                 Soy una persona apasionada de lon que hace, me gusta la technologias, soy un usuario diario de linux, creo en el opensource y su filosofia, disfruto de crear nuevos projectos y darle vida.
                             </li>
-                            <li className="font-bold">
+                            <li className="font-bold text-3xl mb-6">
                                 Skills:
                             </li>
-                            <li className="font-bold">
+                            <li className="font-bold text-3xl mb-6">
                                 Hobbies:
                             </li>
-                            <li className="font-bold">
+                            <li className="font-bold text-3xl mb-6">
                                 Email:
+                            </li>
+                            <li className="font-bold text-3xl mb-6">
+                                Github:
                             </li>
                         </ul>
                     </div>
