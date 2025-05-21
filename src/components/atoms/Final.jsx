e97de85da231c47b5624bce604376c5346179bce
humbertoragazzini <beto_vicio@hotmail.com>
2025-05-21 20:17:57 +0000

    added new style to the end screen

--- src/components/atoms/Final.jsx
+++ src/components/atoms/Final.jsx
@@ -3,13 +3,29 @@ import { Html } from "@react-three/drei";
 export default function Final() {
     return (
         <Html occlude={"blending"} transform position={[0, 0, -1450]}>
-            <div className="w-screen h-screen bg-black">
-                <div className="grid grid-cols-2">
+            <div className="w-screen h-screen bg-white text-black">
+                <div className="grid grid-cols-2 p-14">
                     <div className="col-span-1">
                         <img></img>
                     </div>
                     <div className="col-span-1">
-
+                        <ul>
+                            <li>
+                                Gracias por haber visitado mi portfolio, esto abierto a nuevos desafios y a seguir creciendo, si tenes algun projecto en mente, nos pongamos en contacto y hagamos de ese proejcto una realidad.
+                            </li>
+                            <li>
+                                Soy una persona apasionada de lon que hace, me gusta la technologias, soy un usuario diario de linux, creo en el opensource y su filosofia, disfruto de crear nuevos projectos y darle vida.
+                            </li>
+                            <li className="font-bold">
+                                Skills:
+                            </li>
+                            <li className="font-bold">
+                                Hobbies:
+                            </li>
+                            <li className="font-bold">
+                                Email:
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
