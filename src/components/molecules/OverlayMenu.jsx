664d775ed93f331eeb3a8fe10f0639833fcccef4
humbertoragazzini <beto_vicio@hotmail.com>
2025-05-27 22:26:25 +0000

    added functionality

--- src/components/molecules/OverlayMenu.jsx
+++ src/components/molecules/OverlayMenu.jsx
@@ -2,11 +2,11 @@ export default function OverlayMenu(){
 	return(
 		<div className="fixed z-[999999] h-screen w-screen bg-[rgba(200,50,180,0.65)] flex justify-center items-center">
 	
-			<div>
-				<button className="p-5 m-3 text-2xl font-bold">Start</button>
-				<button className="p-5 m-3 text-2xl font-bold">Settings</button>
-				<button className="p-5 m-3 text-2xl font-bold">Visit my github</button>
-				<button className="p-5 m-3 text-2xl font-bold">Language</button>
+			<div className="flex flex-col items-center justify-center">
+				<button className="p-5 m-3 text-2xl font-bold hover:cursor-pointer" onClick={()=>{alert("work")}}>Start</button>
+				<button className="p-5 m-3 text-2xl font-bold hover:cursor-pointer" onClick={()=>{alert("work")}}>Settings</button>
+				<button className="p-5 m-3 text-2xl font-bold hover:cursor-pointer" onClick={()=>{alert("work")}}>Visit my github</button>
+				<button className="p-5 m-3 text-2xl font-bold hover:cursor-pointer" onClick={()=>{alert("work")}}>Language</button>
 			</div>
 		</div>
 	)
