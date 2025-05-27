943d0d46a0eeabdaa5fab868ff3948f00150201d
humbertoragazzini <beto_vicio@hotmail.com>
2025-05-27 22:12:31 +0000

    added buttons to the menu

--- src/components/molecules/OverlayMenu.jsx
+++ src/components/molecules/OverlayMenu.jsx
@@ -1,5 +1,13 @@
 export default function OverlayMenu(){
 	return(
-		<div className="fixed z-[999999] h-screen w-screen bg-[rgba(200,50,180,0.65)]"></div>
+		<div className="fixed z-[999999] h-screen w-screen bg-[rgba(200,50,180,0.65)] flex justify-center items-center">
+	
+			<div>
+				<button className="p-5 m-3 text-2xl font-bold">Start</button>
+				<button className="p-5 m-3 text-2xl font-bold">Settings</button>
+				<button className="p-5 m-3 text-2xl font-bold">Visit my github</button>
+				<button className="p-5 m-3 text-2xl font-bold">Language</button>
+			</div>
+		</div>
 	)
 }
