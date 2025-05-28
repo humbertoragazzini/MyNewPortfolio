f5e68afabfc93598da3166e8c2af8f02d9357ad9
humbertoragazzini <beto_vicio@hotmail.com>
2025-05-28 19:45:55 +0000

    building main menu

--- src/components/molecules/OverlayMenu.jsx
+++ src/components/molecules/OverlayMenu.jsx
@@ -3,11 +3,11 @@ export default function OverlayMenu(){
 		<div className="fixed z-[999999] h-screen w-screen bg-[rgba(200,50,180,0.65)] flex justify-center items-center">
 	
 			<div className="flex flex-col items-center justify-center">
-				<button className="p-5 m-3 text-2xl font-bold hover:cursor-pointer" onClick={()=>{alert("work")}}>Start</button>
-				<button className="p-5 m-3 text-2xl font-bold hover:cursor-pointer" onClick={()=>{alert("work")}}>Settings</button>
-				<button className="p-5 m-3 text-2xl font-bold hover:cursor-pointer" onClick={()=>{alert("work")}}>Visit my github</button>
-				<button className="p-5 m-3 text-2xl font-bold hover:cursor-pointer" onClick={()=>{alert("work")}}>Language</button>
-			</div>
-		</div>
-	)
+				<button className="p-5 m-3 text-6xl font-bold text-white hover:scale-125 transition-all duration-500 hover:cursor-pointer text-white drop-shadow-[0_0_5px_#fff]" onClick={()=>{alert("work")}}>Start</button>
+				<button className="p-5 m-3 text-6xl font-bold text-white hover:scale-125 transition-all duration-500 hover:cursor-pointer text-white drop-shadow-[0_0_5px_#fff]" onClick={()=>{alert("work")}}>Settings</button>
+				<button className="p-5 m-3 text-6xl font-bold text-white hover:scale-125 transition-all duration-500 hover:cursor-pointer text-white drop-shadow-[0_0_5px_#fff]" onClick={()=>{alert("work")}}>Visit my github</button>
+				<button className="p-5 m-3 text-6xl font-bold text-white hover:scale-125 transition-all duration-500 hover:cursor-pointer text-white drop-shadow-[0_0_5px_#fff]" onClick={()=>{alert("work")}}>Language</button>
+			</div>     
+		</div>   
+	) 
 }
