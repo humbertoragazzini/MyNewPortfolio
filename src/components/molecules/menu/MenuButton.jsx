079324cffa73c8c8344a1eb42d5d96fd643ec653
humberto ragazzini <beto_vicio@hotmail.com>
2025-05-29 20:26:40 +0200

    added small changes to the menu

--- src/components/molecules/menu/MenuButton.jsx
+++ src/components/molecules/menu/MenuButton.jsx
@@ -1,7 +1,11 @@
-import Button from "../../atoms/ui/Button"
+import Button from "../../atoms/ui/Button";
 
 export default function MenuButton({ text, onClick }) {
-    return (
-        <Button className="p-5 m-3 text-6xl orbitron font-[600] hover:scale-125 transition-all duration-500 hover:cursor-pointer text-white drop-shadow-[0_0_5px_#fff]" onClick={onClick} text={text} ></Button >
-    )
+  return (
+    <Button
+      className="p-3 lg:p-5 lg:m-3 text-3xl md:text-4xl xl:text-6xl orbitron font-[600] hover:scale-125 transition-all duration-500 hover:cursor-pointer text-white drop-shadow-[0_0_5px_#fff]"
+      onClick={onClick}
+      text={text}
+    ></Button>
+  );
 }
