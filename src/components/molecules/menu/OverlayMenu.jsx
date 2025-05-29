121356a08a752e2ae54e35a0b0fc4fe48f291f90
humberto ragazzini <beto_vicio@hotmail.com>
2025-05-29 19:55:50 +0200

    adding menu functions

--- src/components/molecules/menu/OverlayMenu.jsx
+++ src/components/molecules/menu/OverlayMenu.jsx
@@ -3,34 +3,53 @@ import { AppContext } from "../../../context/AppContext";
 import MenuButton from "./MenuButton";
 
 export default function OverlayMenu() {
+  const { toggleLanguage, isMenuOpen } = useContext(AppContext);
 
-	const { toggleLanguage } =
-		useContext(AppContext);
-
-	return (
-		<div className="fixed z-[9999] h-screen w-screen bg-[rgba(200,50,180,0.65)] flex justify-center items-center overflow-hidden">
-
-			<div className="flex flex-col items-center justify-center">
-				<MenuButton onClick={() => { console.log("click menu") }} text={{
-					en: "START",
-					es: "COMENZAR"
-				}}></MenuButton>
-				<MenuButton onClick={() => { console.log("click menu") }} text={{
-					en: "SETTINGS",
-					es: "CONFIGURACION"
-				}}></MenuButton>
-				<MenuButton onClick={() => {
-					console.log("working")
-					toggleLanguage()
-				}} text={{
-					en: "LANGUAGE",
-					es: "IDIOMA"
-				}}></MenuButton >
-				<MenuButton onClick={() => { console.log("click menu") }} text={{
-					en: "GITHUB",
-					es: "GITHUB"
-				}}></MenuButton>
-			</div >
-		</div >
-	)
+  return (
+    <div
+      className={`fixed ${
+        isMenuOpen ? "transform-y-0" : "transform-y-[120vh]"
+      } z-[9999] h-screen w-screen bg-[rgba(200,50,180,0.65)] flex justify-center items-center overflow-hidden`}
+    >
+      <div className="flex flex-col items-center justify-center">
+        <MenuButton
+          onClick={() => {
+            setIsMenuOpen(false);
+          }}
+          text={{
+            en: "START",
+            es: "COMENZAR",
+          }}
+        ></MenuButton>
+        <MenuButton
+          onClick={() => {
+            console.log("click menu");
+          }}
+          text={{
+            en: "SETTINGS",
+            es: "CONFIGURACION",
+          }}
+        ></MenuButton>
+        <MenuButton
+          onClick={() => {
+            console.log("working");
+            toggleLanguage();
+          }}
+          text={{
+            en: "LANGUAGE",
+            es: "IDIOMA",
+          }}
+        ></MenuButton>
+        <MenuButton
+          onClick={() => {
+            console.log("click menu");
+          }}
+          text={{
+            en: "GITHUB",
+            es: "GITHUB",
+          }}
+        ></MenuButton>
+      </div>
+    </div>
+  );
 }
