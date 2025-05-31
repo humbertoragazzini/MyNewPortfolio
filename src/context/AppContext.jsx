7bf02f8c4006a68a01eb8882540bd868da6367f8
humbertoragazzini <beto_vicio@hotmail.com>
2025-05-31 10:07:36 +0000

    working on quality settings

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,15 +1,19 @@
 import React, { createContext, useState } from "react";
+import {RGBA_ASTC_4x4_Format} from "three";
 
 export const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(true);
   const [language, setLanguage] = useState("en");
-
+  const [reflections,setReflections] = useState();
+  const [reflectionQuality, setReflectionQuality] = useState(512);
   // Example of toggling values
   const toggleMenu = () => setIsMenuOpen((prev) => !prev);
   const toggleLanguage = () =>
     setLanguage((prev) => (prev === "en" ? "es" : "en"));
+  const toggleReflections = ()=>setReflections((prev)=>!prev)
+  const changeReflectionQuality = (quality)=>{setReflectionQuality(quality)}
 
   return (
     <AppContext.Provider
@@ -19,6 +23,8 @@ export const AppProvider = ({ children }) => {
         language,
         setLanguage,
         toggleLanguage,
+        toggleReflections,
+        changeReflectionQuality
       }}
     >
       {children}
