62c3a393f394ab98acb0e12ad2a630af588fcab7
humbertoragazzini <beto_vicio@hotmail.com>
2025-06-03 20:34:57 +0000

    added again the document pages

--- src/components/organisms/Experience.jsx
+++ src/components/organisms/Experience.jsx
@@ -60,7 +60,7 @@ export default function Experience() {
                   </MovingMap>
                 </Environment>)
               } */}
-              {/* 
+
               <IframedLeft
                 positionZ={-40}
                 url={"https://www.primalports.com/"}
@@ -72,11 +72,11 @@ export default function Experience() {
               <IframedLeft
                 positionZ={-245}
                 url={"https://humbertoragazzini.github.io/thewebglglobe/dist/"}
-              /> */}
-              {/* <ProjectRight positionZ={-40} />
+              />
+              <ProjectRight positionZ={-40} />
               <ProjectLeft positionZ={-145} />
               <ProjectRight positionZ={-245} />
-              <Final></Final> */}
+              <Final></Final>
               {/* <OrbitControls></OrbitControls> */}
               {/* <PerspectiveCamera></PerspectiveCamera> */}
               <Camera scroll={scrollRef}></Camera>
