28012778f140d05fe9b8b36728446b5d8f9927ce
humbertoragazzini <beto_vicio@hotmail.com>
2025-06-08 20:18:03 +0000

    removed the if statement in reflection

--- src/components/atoms/scene/Effects.jsx
+++ src/components/atoms/scene/Effects.jsx
@@ -25,33 +25,31 @@ export default function Effects({
     } = useContext(AppContext);
 
     useFrame((state) => {
-        if (reflections) {
-            if (cameraRef.current !== undefined) {
-                const cubeIndex = cameraRef.current.children.findIndex(
-                    (obj) => obj?.constructor?.name === 'CubeCamera'
-                )
-                const cube = cameraRef.current.children[cubeIndex];
-                if (cubeIndex !== undefined && scroll.current !== undefined) {
-                    cube.position.z = THREE.MathUtils.lerp(
-                        cube.position.z,
-                        75 - 1 * 1492 * scroll.current,
-                        lerpSpeed
-                    );
+        if (cameraRef.current !== undefined) {
+            const cubeIndex = cameraRef.current.children.findIndex(
+                (obj) => obj?.constructor?.name === 'CubeCamera'
+            )
+            const cube = cameraRef.current.children[cubeIndex];
+            if (cubeIndex !== undefined && scroll.current !== undefined) {
+                cube.position.z = THREE.MathUtils.lerp(
+                    cube.position.z,
+                    75 - 1 * 1492 * scroll.current,
+                    lerpSpeed
+                );
 
-                    // save current renderer state
-                    const currentRenderTarget = state.gl.getRenderTarget();
-                    const currentAutoClear = state.gl.autoClear;
+                // save current renderer state
+                const currentRenderTarget = state.gl.getRenderTarget();
+                const currentAutoClear = state.gl.autoClear;
 
-                    state.gl.setRenderTarget(null); // render to screen (not composer target)
-                    state.gl.autoClear = true;      // make sure framebuffer is cleared
+                state.gl.setRenderTarget(null); // render to screen (not composer target)
+                state.gl.autoClear = true;      // make sure framebuffer is cleared
 
-                    // this is the critical fix: render only the scene to update the cubemap
-                    cube.update(state.gl, state.scene);
+                // this is the critical fix: render only the scene to update the cubemap
+                cube.update(state.gl, state.scene);
 
-                    // restore
-                    state.gl.setRenderTarget(currentRenderTarget);
-                    state.gl.autoClear = currentAutoClear;
-                }
+                // restore
+                state.gl.setRenderTarget(currentRenderTarget);
+                state.gl.autoClear = currentAutoClear;
             }
         }
     }, 0);
