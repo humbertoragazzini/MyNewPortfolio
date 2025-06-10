a933d69e435ecc183c4cc619dfe1a307596f359e
humbertoragazzini <beto_vicio@hotmail.com>
2025-06-10 21:15:41 +0000

    checking variables of cubecamera

--- src/components/atoms/scene/Effects.jsx
+++ src/components/atoms/scene/Effects.jsx
@@ -29,7 +29,9 @@ export default function Effects({
                 (obj) => obj?.constructor?.name === 'CubeCamera' || obj?.constructor?.name === "HR"
             )
             const cube = cameraRef.current.children[cubeIndex];
-
+            console.log(cube)
+            console.log(cameraRef.current)
+            console.log(cameraRef.current.children)
             if (cube !== undefined && cubeIndex !== undefined && scroll.current !== undefined) {
                 cube.position.z = THREE.MathUtils.lerp(
                     cube.position.z,
