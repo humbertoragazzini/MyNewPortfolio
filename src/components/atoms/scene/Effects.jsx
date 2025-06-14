5f2b13b429f79c9aa990c2ed736ab12cc95bc84f
humbertoragazzini <beto_vicio@hotmail.com>
2025-06-14 21:27:16 +0000

    added some small tweaks

--- src/components/atoms/scene/Effects.jsx
+++ src/components/atoms/scene/Effects.jsx
@@ -3,7 +3,7 @@ import { useFrame } from "@react-three/fiber";
 import { useRef, useState, useContext } from "react";
 import * as THREE from "three";
 import { AppContext } from "../../../context/AppContext";
-import { EffectComposer, Bloom, DepthOfField, Noise, Vignette } from '@react-three/postprocessing';
+import { EffectComposer, Bloom, DepthOfField, Noise, Vignette, SSAO } from '@react-three/postprocessing';
 import { BlendFunction } from 'postprocessing';
 
 export default function Effects({
@@ -76,13 +76,15 @@ export default function Effects({
 
             {!reflections && children()}
             {
-                postProcessing && <EffectComposer>
+                postProcessing && <EffectComposer normalPass>
                     <Bloom
                         intensity={1}
                         luminanceThreshold={0.4}
                         luminanceSmoothing={0.9}
                         blendFunction={BlendFunction.SCREEN}
                     />
+
+                    <DepthOfField focusDistance={0.02} focalLength={0.02} bokehScale={2} height={480} />
                 </EffectComposer>
             }
 
