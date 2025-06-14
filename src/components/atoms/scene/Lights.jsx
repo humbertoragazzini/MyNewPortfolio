7cef1c97a50b3e964f9776f14172f1bd84c4cb88
humbertoragazzini <beto_vicio@hotmail.com>
2025-06-14 19:21:33 +0000

    added moving gsap animation

--- src/components/atoms/scene/Lights.jsx
+++ src/components/atoms/scene/Lights.jsx
@@ -62,17 +62,17 @@ export default function Lights({ targetRef }) {
     <group>
       <ambientLight ref={ambientRef} intensity={0.25}></ambientLight>
       {/* Stroboscopic */}
-      <Stroboscopic lightValues={lightValues} index={0} delay={0.10} position={[0, 0, 0]}></Stroboscopic>
+      {/* <Stroboscopic lightValues={lightValues} index={0} delay={0.10} position={[0, 0, 0]}></Stroboscopic> */}
       {/* <Stroboscopic lightValues={lightValues} index={1} delay={0.20} position={[0, 0, 100]}></Stroboscopic> */}
-      <Stroboscopic lightValues={lightValues} index={2} delay={0.30} position={[0, 0, 200]}></Stroboscopic>
+      {/* <Stroboscopic lightValues={lightValues} index={2} delay={0.30} position={[0, 0, 200]}></Stroboscopic> */}
       {/* <Stroboscopic lightValues={lightValues} index={3} delay={0.4} position={[0, 0, 300]}></Stroboscopic> */}
-      <Stroboscopic lightValues={lightValues} index={4} delay={0.5} position={[0, 0, 400]}></Stroboscopic>
+      {/* <Stroboscopic lightValues={lightValues} index={4} delay={0.5} position={[0, 0, 400]}></Stroboscopic> */}
       {/* <Stroboscopic lightValues={lightValues} index={5} delay={0.6} position={[0, 0, 500]}></Stroboscopic> */}
-      <Stroboscopic lightValues={lightValues} index={6} delay={0.7} position={[0, 0, 600]}></Stroboscopic>
+      {/* <Stroboscopic lightValues={lightValues} index={6} delay={0.7} position={[0, 0, 600]}></Stroboscopic> */}
       {/* <Stroboscopic lightValues={lightValues} index={7} delay={0.8} position={[0, 0, 700]}></Stroboscopic> */}
-      <Stroboscopic lightValues={lightValues} index={8} delay={0.9} position={[0, 0, 800]}></Stroboscopic>
+      {/* <Stroboscopic lightValues={lightValues} index={8} delay={0.9} position={[0, 0, 800]}></Stroboscopic> */}
       {/* Moving */}
-      <MovingLight></MovingLight>
+      <MovingLight position={[0, 0, 0]}></MovingLight>
     </group>
   );
 }
