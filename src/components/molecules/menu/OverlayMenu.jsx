8dd010b4ec400a9ae3882f3e02b95f139c7750a5
humberto ragazzini <beto_vicio@hotmail.com>
2025-06-22 20:23:34 +0200

    working on the loading system interface

--- src/components/molecules/menu/OverlayMenu.jsx
+++ src/components/molecules/menu/OverlayMenu.jsx
@@ -3,6 +3,7 @@ import { AppContext } from "../../../context/AppContext";
 import MenuButton from "./MenuButton";
 import { motion } from "framer-motion";
 import { LuMenu } from "react-icons/lu";
+import LoadingSystems from "../../atoms/ui/LoadingSystems";
 
 export default function OverlayMenu() {
   const {
@@ -28,107 +29,7 @@ export default function OverlayMenu() {
 
   return (
     <>
-      <div className="h-[calc(100%-15px)] m-2 bg-[rgba(0,0,0,0.55)] rounded-2xl orbitron fixed w-[280px] top-0 left-0 z-[9999] p-4 text-[rgba(255,255,255,1)] overflow-hidden">
-        <div className="flex items-center justify-start my-1">
-          <span className="inline-block mr-2 w-4 h-4 border-[1px] bg-amber-800 border-amber-400 opacity-40"></span>
-          <p>Initializing core systems...</p>
-        </div>
-
-        <div className="flex items-center justify-start my-1">
-          <span className="inline-block mr-2 w-4 h-4 border-[1px] bg-amber-800 border-amber-400 opacity-40"></span>
-          <p>Power plant online.</p>
-        </div>
-
-        <div className="flex items-center justify-start my-1">
-          <span className="inline-block mr-2 w-4 h-4 border-[1px] bg-amber-800 border-amber-400 opacity-40"></span>
-          <p>Main reactor stabilized.</p>
-        </div>
-
-        <div className="flex items-center justify-start my-1">
-          <span className="inline-block mr-2 w-4 h-4 border-[1px] bg-amber-800 border-amber-400 opacity-40"></span>
-          <p>Life support systems nominal.</p>
-        </div>
-
-        <div className="flex items-center justify-start my-1">
-          <span className="inline-block mr-2 w-4 h-4 border-[1px] bg-amber-800 border-amber-400 opacity-40"></span>
-          <p>Navigation systems calibrated.</p>
-        </div>
-
-        <div className="flex items-center justify-start my-1">
-          <span className="inline-block mr-2 w-4 h-4 border-[1px] bg-amber-800 border-amber-400 opacity-40"></span>
-          <p>Starboard thrusters responsive.</p>
-        </div>
-
-        <div className="flex items-center justify-start my-1">
-          <span className="inline-block mr-2 w-4 h-4 border-[1px] bg-amber-800 border-amber-400 opacity-40"></span>
-          <p>Portside stabilizers aligned.</p>
-        </div>
-
-        <div className="flex items-center justify-start my-1">
-          <span className="inline-block mr-2 w-4 h-4 border-[1px] bg-amber-800 border-amber-400 opacity-40"></span>
-          <p>Communication array activated.</p>
-        </div>
-
-        <div className="flex items-center justify-start my-1">
-          <span className="inline-block mr-2 w-4 h-4 border-[1px] bg-amber-800 border-amber-400 opacity-40"></span>
-          <p>Sensor grid fully operational.</p>
-        </div>
-
-        <div className="flex items-center justify-start my-1">
-          <span className="inline-block mr-2 w-4 h-4 border-[1px] bg-amber-800 border-amber-400 opacity-40"></span>
-          <p>Artificial gravity engaged.</p>
-        </div>
-
-        <div className="flex items-center justify-start my-1">
-          <span className="inline-block mr-2 w-4 h-4 border-[1px] bg-amber-800 border-amber-400 opacity-40"></span>
-          <p>Thermal regulators optimal.</p>
-        </div>
-
-        <div className="flex items-center justify-start my-1">
-          <span className="inline-block mr-2 w-4 h-4 border-[1px] bg-amber-800 border-amber-400 opacity-40"></span>
-          <p>Flight control interface ready.</p>
-        </div>
-
-        <div className="flex items-center justify-start my-1">
-          <span className="inline-block mr-2 w-4 h-4 border-[1px] bg-amber-800 border-amber-400 opacity-40"></span>
-          <p>Shields at 100% capacity.</p>
-        </div>
-
-        <div className="flex items-center justify-start my-1">
-          <span className="inline-block mr-2 w-4 h-4 border-[1px] bg-amber-800 border-amber-400 opacity-40"></span>
-          <p>Weapon systems on standby.</p>
-        </div>
-
-        <div className="flex items-center justify-start my-1">
-          <span className="inline-block mr-2 w-4 h-4 border-[1px] bg-amber-800 border-amber-400 opacity-40"></span>
-          <p>Cargo bay secured.</p>
-        </div>
-
-        <div className="flex items-center justify-start my-1">
-          <span className="inline-block mr-2 w-4 h-4 border-[1px] bg-amber-800 border-amber-400 opacity-40"></span>
-          <p>Cryo-pods status: green.</p>
-        </div>
-
-        <div className="flex items-center justify-start my-1">
-          <span className="inline-block mr-2 w-4 h-4 border-[1px] bg-amber-800 border-amber-400 opacity-40"></span>
-          <p>Environmental seal confirmed.</p>
-        </div>
-
-        <div className="flex items-center justify-start my-1">
-          <span className="inline-block mr-2 w-4 h-4 border-[1px] bg-amber-800 border-amber-400 opacity-40"></span>
-          <p>Data uplink to command center successful.</p>
-        </div>
-
-        <div className="flex items-center justify-start my-1">
-          <span className="inline-block mr-2 w-4 h-4 border-[1px] bg-amber-800 border-amber-400 opacity-40"></span>
-          <p>Captain’s interface online.</p>
-        </div>
-
-        <div className="flex items-center justify-start my-1 font-semibold text-green-500">
-          <span className="inline-block mr-2 w-4 h-4 border-[1px] bg-green-700 border-green-400 opacity-80 animate-pulse"></span>
-          <p>All systems green. Ready for departure.</p>
-        </div>
-      </div>
+      <LoadingSystems></LoadingSystems>
       <div className="fixed w-[280px] bottom-0 right-0 z-[9999] p-4 bg-[rgba(66.6,0.179,58.318,0.4)] flex justify-end items-end">
         <div className="w-[280px] flex items-end justify-end flex-col">
           <div className="life h-[25px] bg-amber-500 w-4/5 mb-1"></div>
