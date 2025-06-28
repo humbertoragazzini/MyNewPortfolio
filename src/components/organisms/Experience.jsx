8603bcbc07ed4acf8febbf366920a5a642b898d8
humbertoragazzini <beto_vicio@hotmail.com>
2025-06-28 20:51:59 +0000

    adding project

--- src/components/organisms/Experience.jsx
+++ src/components/organisms/Experience.jsx
@@ -72,12 +72,12 @@ const projects = [
     projectId: "firstRight",
     orientation: "right",
     projectName: {
-      en: "My first project as a web developer",
-      es: "Mi primer project como desarrollador web",
+      en: "The second is spacial too on my dev journey",
+      es: "Mi segundo projecto tambien es especial en mi camino a ser un desarrollador",
     },
     description: {
-      en: "This was the first project I created in HTML, with CSS and a bit of JavaScript. This project was the beginning of what is now my career. It’s impressive to look at this project and know that everything started here. This project holds great sentimental value because I can come back to it and see how my knowledge has grown over time and with a lot of effort.",
-      es: "Este fue el primer projecto que realize en HTML, con CSS y un poco the Javascript, este projecto fue el comienzo de lo que hoy es mi carrera, es impresionante ver este projecto y saber que todo empezo aqui, este projecto tiene ese gran valor sentimental de poder volver a el y ver como mi conocimiento crecio con tiempo y mucho esfuerzo.",
+      en: "In this second project, I dove deeper into JavaScript. I created a small memory game where you need to repeat the same sequence of jewels to complete a bracelet. This game marked the beginning of my JavaScript journey and sparked my desire to learn more.",
+      es: "En este segundo proyecto profundicé en JavaScript. Creé un pequeño juego de memoria en el que debes repetir la misma secuencia de joyas para completar una pulsera. Este juego marcó el comienzo de mi camino con JavaScript y despertó mis ganas de aprender más.",
     },
     technologies: [
       "Bootstrap",
@@ -92,10 +92,10 @@ const projects = [
           es: "Link al projecto",
         },
         label: {
-          en: "Click to visit my first project",
-          es: "Clieckea aqui para visitar mi primer projecto",
+          en: "Click to visit The World Bracelet Championship",
+          es: "Clieckea aqui para visitar The World Bracelet Championship",
         },
-        url: "https://humbertoragazzini.github.io/ms-1-front-end/",
+        url: "https://humbertoragazzini.github.io/the-world-bracelet-championship-ms2/",
       },
       {
         title: {
@@ -106,7 +106,7 @@ const projects = [
           en: "Github Repository",
           es: "Repositorio de Github",
         },
-        url: "https://humbertoragazzini.github.io/ms-1-front-end/",
+        url: "https://github.com/humbertoragazzini/the-world-bracelet-championship-ms2",
       }
     ],
   },
@@ -115,61 +115,57 @@ const projects = [
     projectId: "secondLeft",
     orientation: "left",
     projectName: {
-      en: "My first project as a web developer",
-      es: "Mi primer project como desarrollador web",
+      en: "Splendid Pizza the first full stack-project",
+      es: "Splendid Pizza mi primer projecto full-stack",
     },
     description: {
-      en: "This was the first project I created in HTML, with CSS and a bit of JavaScript. This project was the beginning of what is now my career. It’s impressive to look at this project and know that everything started here. This project holds great sentimental value because I can come back to it and see how my knowledge has grown over time and with a lot of effort.",
-      es: "Este fue el primer projecto que realize en HTML, con CSS y un poco the Javascript, este projecto fue el comienzo de lo que hoy es mi carrera, es impresionante ver este projecto y saber que todo empezo aqui, este projecto tiene ese gran valor sentimental de poder volver a el y ver como mi conocimiento crecio con tiempo y mucho esfuerzo.",
+      en: "Unfortunately, this project went offline due to changes in MongoDB, but it was my first full-stack project, where I combined a database for all the recipes with an interface to add, remove, and view different types of pizzas along with their recipes.",
+      es: "Desafortunadamente, este proyecto dejó de estar en línea debido a cambios en MongoDB, pero fue mi primer proyecto full-stack, en el que se combinó una base de datos para todas las recetas y una interfaz para agregar, quitar y visualizar los distintos tipos de pizzas con sus recetas.",
     },
     technologies: [
       "Bootstrap",
       "HTML5",
       "CSS",
       "Javascript",
+      "Python",
+      "MongoDB",
+      "Flask",
     ],
-    links: [
-      {
-        title: {
-          en: "Link to the project",
-          es: "Link al projecto",
-        },
-        label: {
-          en: "Click to visit my first project",
-          es: "Clieckea aqui para visitar mi primer projecto",
-        },
-        url: "https://humbertoragazzini.github.io/ms-1-front-end/",
+    links: [{
+      title: {
+        en: "Link to the Github repository",
+        es: "Link al repositorio de Github",
       },
-      {
-        title: {
-          en: "Link to the Github repository",
-          es: "Link al repositorio de Github",
-        },
-        label: {
-          en: "Github Repository",
-          es: "Repositorio de Github",
-        },
-        url: "https://humbertoragazzini.github.io/ms-1-front-end/",
-      }
-    ],
+      label: {
+        en: "Github Repository",
+        es: "Repositorio de Github",
+      },
+      url: "https://github.com/humbertoragazzini/splendid-the-pizza-cooking-book",
+    }],
   },
   {
     positionZ: -145,
     projectId: "secondRight",
     orientation: "right",
     projectName: {
-      en: "My first project as a web developer",
-      es: "Mi primer project como desarrollador web",
+      en: "PrimalPort - Export and imports",
+      es: "PrimalPort - Exportaciones e importaciones",
     },
     description: {
-      en: "This was the first project I created in HTML, with CSS and a bit of JavaScript. This project was the beginning of what is now my career. It’s impressive to look at this project and know that everything started here. This project holds great sentimental value because I can come back to it and see how my knowledge has grown over time and with a lot of effort.",
-      es: "Este fue el primer projecto que realize en HTML, con CSS y un poco the Javascript, este projecto fue el comienzo de lo que hoy es mi carrera, es impresionante ver este projecto y saber que todo empezo aqui, este projecto tiene ese gran valor sentimental de poder volver a el y ver como mi conocimiento crecio con tiempo y mucho esfuerzo.",
+      en: "PrimalPort is, without a doubt, the project where I combine my knowledge of WebGL, React, Next.js, and animations using Framer and GSAP. This project was developed for a company dedicated to the import and export of goods.",
+      es: "PrimalPort es, sin duda, el proyecto en el que combino mis conocimientos en WebGL, React, Next.js y animaciones con Framer y GSAP. Este proyecto fue desarrollado para una empresa dedicada a la importación y exportación de bienes.",
     },
     technologies: [
       "Bootstrap",
       "HTML5",
       "CSS",
-      "Javascript",
+      "TS",
+      "NextJS",
+      "GSAP",
+      "Framer",
+      "RTF",
+      "Blender",
+      "TailwindCSS"
     ],
     links: [
       {
@@ -178,21 +174,10 @@ const projects = [
           es: "Link al projecto",
         },
         label: {
-          en: "Click to visit my first project",
-          es: "Clieckea aqui para visitar mi primer projecto",
+          en: "Click here to go to PrimalPort",
+          es: "Clickea aqui para visitar PrimalPort",
         },
-        url: "https://humbertoragazzini.github.io/ms-1-front-end/",
-      },
-      {
-        title: {
-          en: "Link to the Github repository",
-          es: "Link al repositorio de Github",
-        },
-        label: {
-          en: "Github Repository",
-          es: "Repositorio de Github",
-        },
-        url: "https://humbertoragazzini.github.io/ms-1-front-end/",
+        url: "https://www.primalports.com/",
       }
     ],
   },
@@ -201,12 +186,12 @@ const projects = [
     projectId: "thirdLeft",
     orientation: "left",
     projectName: {
-      en: "My first project as a web developer",
-      es: "Mi primer project como desarrollador web",
+      en: "Starting with WebGL and RTF",
+      es: "Empezando con WebGL y RTF",
     },
     description: {
-      en: "This was the first project I created in HTML, with CSS and a bit of JavaScript. This project was the beginning of what is now my career. It’s impressive to look at this project and know that everything started here. This project holds great sentimental value because I can come back to it and see how my knowledge has grown over time and with a lot of effort.",
-      es: "Este fue el primer projecto que realize en HTML, con CSS y un poco the Javascript, este projecto fue el comienzo de lo que hoy es mi carrera, es impresionante ver este projecto y saber que todo empezo aqui, este projecto tiene ese gran valor sentimental de poder volver a el y ver como mi conocimiento crecio con tiempo y mucho esfuerzo.",
+      en: "This is just an example of a WebGL game. It’s not finished — I just wanted to create a small game where you have to jump and navigate different obstacles to reach the donuts.",
+      es: "Esto es solo un ejemplo de un juego en WebGL. No está terminado; simplemente quería crear un pequeño juego en el que tengas que saltar y sortear diferentes obstáculos para alcanzar las donas.",
     },
     technologies: [
       "Bootstrap",
