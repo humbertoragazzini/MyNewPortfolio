00e80e79b135727dec72aabdf0a004fa2cd046e6
humbertoragazzini <beto_vicio@hotmail.com>
2025-06-28 23:13:41 +0000

    changed the color to red

--- src/components/atoms/scene/ProjectLeft.jsx
+++ src/components/atoms/scene/ProjectLeft.jsx
@@ -68,7 +68,7 @@ export default function ProjectLeft({ positionZ, projectId, children }) {
             transform
           >
             <div
-              className={`relative bg-gradient-to-br from-purple-600 to-lime-900 ${selectedProject
+              className={`relative bg-gradient-to-br from-purple-600 to-red-900 ${selectedProject
                 ? selectedProject == projectId
                   ? ""
                   : "blur-md"
