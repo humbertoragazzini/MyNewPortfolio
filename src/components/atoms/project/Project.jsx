b0b5ee49520a75739e7b8df8983348a41fb4950b
humbertoragazzini <beto_vicio@hotmail.com>
2025-07-01 19:18:22 +0000

    the format mkv is not widly supported changed to other format

--- src/components/atoms/project/Project.jsx
+++ src/components/atoms/project/Project.jsx
@@ -53,7 +53,7 @@ export default function Project({ language, content, changeSelectedProject }) {
                 loop
                 playsInline
               >
-                <source src="./videos/primalport.mkv" type="video/mp4" />
+                <source src="./videos/Testing_Video_h_264.mp4" type="video/mp4" />
                 Your browser does not support the video tag.
               </video>
             </div>
