4d7740a3cba20849e8a92f204acdd49c94c62c42
humberto ragazzini <beto_vicio@hotmail.com>
2025-07-02 19:53:28 +0200

    added new material starting the shader

--- src/components/atoms/scene/TextShader.jsx
+++ src/components/atoms/scene/TextShader.jsx
@@ -1,13 +1,14 @@
 import { Text } from "@react-three/drei";
+import * as THREE from "three";
 
 export default function TextShader({ text }) {
   return (
     <Text
       position={[0, 15, 0]}
       scale={5}
-      color="blue"
       anchorX="center"
       anchorY="middle"
+      material={new THREE.MeshStandardMaterial({ color: "red" })}
     >
       {text}
     </Text>
