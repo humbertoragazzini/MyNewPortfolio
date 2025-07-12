94891fe897bfb9eadcda75e2d83958f0297c0dbb
humberto ragazzini <beto_vicio@hotmail.com>
2025-07-12 17:15:27 +0200

    added buttons to the carousel

--- src/components/molecules/ui/MultiCarousel.tsx
+++ src/components/molecules/ui/MultiCarousel.tsx
@@ -1,6 +1,7 @@
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import Heading from "../../atoms/ui/Heading";
+import React from "react";
 
 export default function MultiCarousel({ language }) {
   const responsive = {
@@ -22,6 +23,84 @@ export default function MultiCarousel({ language }) {
       items: 1,
     },
   };
+  const carouselItems = [
+    <div className="w-full px-3 text-white py-9">
+      <div className="flex flex-col items-center justify-center col-span-12 lg:col-span-6 xl:col-span-4">
+        <Heading
+          language={language}
+          className={
+            "text-white font-semibold drop-shadow-[0_0_5px_#fff] text-center mb-6"
+          }
+          level={1}
+          text={{
+            en: "Touch controls",
+            es: "Controles tactiles",
+          }}
+        ></Heading>
+        <button className="w-32 mx-auto mb-3">
+          <img className="w-32" src="img/touch.svg"></img>
+        </button>
+      </div>
+    </div>,
+    <div className="w-full px-3 text-white py-9">
+      <div className="flex flex-col items-center justify-center col-span-12 lg:col-span-6 xl:col-span-4">
+        <Heading
+          language={language}
+          className={
+            "text-white font-semibold drop-shadow-[0_0_5px_#fff] text-center mb-6"
+          }
+          level={1}
+          text={{
+            en: "On screen joysticks",
+            es: "Controles en pantalla",
+          }}
+        ></Heading>
+        <button className="w-32 mx-auto mb-3">
+          <img className="w-32" src="img/joystick.svg"></img>
+        </button>
+      </div>
+    </div>,
+    <div className="w-full px-3 text-white py-9">
+      <div className="flex flex-col items-center justify-center col-span-12 lg:col-span-6 xl:col-span-4">
+        <Heading
+          language={language}
+          className={
+            "text-white font-semibold drop-shadow-[0_0_5px_#fff] text-center mb-6"
+          }
+          level={1}
+          text={{
+            en: "Regular mouse web browsing",
+            es: "Regular mouse web browsing",
+          }}
+        ></Heading>
+        <button className="w-32 mx-auto mb-3">
+          <img className="w-32" src="img/mouse.svg"></img>
+        </button>
+      </div>
+    </div>,
+  ];
+
+  function CustomDot({ onClick, ...rest }) {
+    const {
+      onMove,
+      index,
+      active,
+      carouselState: { currentSlide, deviceType },
+    } = rest;
+    // onMove means if dragging or swiping in progress.
+    // active is provided by this lib for checking if the item is active or not.
+    return (
+      <button onClick={() => onClick()}>
+        {/* {React.Children.toArray(carouselItems)[index]} */}
+        <div
+          className={`w-4 h-4 m-1 border-2 border-white rounded-full ${
+            active ? "bg-[rgba(255,255,255,0.25)]" : "bg-black"
+          }`}
+        ></div>
+      </button>
+    );
+  }
+
   return (
     <Carousel
       responsive={responsive}
@@ -29,55 +108,9 @@ export default function MultiCarousel({ language }) {
       infinite={true}
       slidesToSlide={1}
       removeArrowOnDeviceType={["tablet", "mobile", "desktop"]}
+      customDot={<CustomDot />}
     >
-      <div className="w-full px-3 text-white py-9">
-        <div className="col-span-12 lg:col-span-6 xl:col-span-4">
-          <Heading
-            language={language}
-            className={
-              "text-white font-semibold drop-shadow-[0_0_5px_#fff] text-center mb-6"
-            }
-            level={1}
-            text={{
-              en: "Touch controls",
-              es: "Controles tactiles",
-            }}
-          ></Heading>
-          <img className="w-32 mx-auto mb-3" src="img/touch.svg"></img>
-        </div>
-      </div>
-      <div className="w-full px-3 text-white py-9">
-        <div className="col-span-12 lg:col-span-6 xl:col-span-4">
-          <Heading
-            language={language}
-            className={
-              "text-white font-semibold drop-shadow-[0_0_5px_#fff] text-center mb-6"
-            }
-            level={1}
-            text={{
-              en: "On screen joysticks",
-              es: "Controles en pantalla",
-            }}
-          ></Heading>
-          <img className="w-32 mx-auto mb-3" src="img/joystick.svg"></img>
-        </div>
-      </div>
-      <div className="w-full px-3 text-white py-9">
-        <div className="col-span-12 lg:col-span-6 xl:col-span-4">
-          <Heading
-            language={language}
-            className={
-              "text-white font-semibold drop-shadow-[0_0_5px_#fff] text-center mb-6"
-            }
-            level={1}
-            text={{
-              en: "Regular mouse web browsing",
-              es: "Regular mouse web browsing",
-            }}
-          ></Heading>
-          <img className="w-32 mx-auto mb-3" src="img/mouse.svg"></img>
-        </div>
-      </div>
+      {carouselItems}
     </Carousel>
   );
 }
