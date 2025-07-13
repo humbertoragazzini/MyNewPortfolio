254f02a719ce5ee61b4a71b90b168ab0851aa82d
humberto ragazzini <beto_vicio@hotmail.com>
2025-07-13 16:42:51 +0200

    added some style to the menu buttons

--- src/components/molecules/menu/OverlayMenu.jsx
+++ src/components/molecules/menu/OverlayMenu.jsx
@@ -34,7 +34,11 @@ export default function OverlayMenu() {
 
   return (
     <>
-      <div className="fixed bottom-0 right-0 z-[9999] p-4 bg-[rgba(66.6,0.179,58.318,0.4)] flex justify-end items-end">
+      <div
+        className={`${
+          isMenuOpen ? "-translate-y-[calc(100%+50px)]" : "translate-y-0"
+        } fixed duration-500 ease-initial  top-0 right-0 z-[9999] bg-[rgba(66.6,0.179,58.318,0.4)] flex justify-end items-end`}
+      >
         {/* <div className="w-[280px] flex items-end justify-end flex-col">
           <div className="life h-[25px] bg-amber-500 w-4/5 mb-1"></div>
           <div className="shield h-[15px] bg-blue-800 w-3/4 mb-1"></div>
@@ -45,9 +49,7 @@ export default function OverlayMenu() {
             toggleMenu();
             console.log("click menu");
           }}
-          className={`m-2 rounded-2xl right-[0px] duration-500 ease-initial cursor-pointer ${
-            isMenuOpen ? "-translate-y-[calc(100%+50px)]" : "translate-y-0"
-          }`}
+          className={`rounded-2xl p-4 right-[0px] cursor-pointer`}
         >
           <LuMenu className="relative z-[1000] w-[35px] h-[35px] stroke-white pointer-events-none"></LuMenu>
         </button>
