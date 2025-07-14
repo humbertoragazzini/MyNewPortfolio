417131486838e8f07332bdf21778eb7ded03dba7
humberto ragazzini <beto_vicio@hotmail.com>
2025-07-14 21:29:23 +0200

    start changing buttons

--- src/components/molecules/menu/IntroMenu.jsx
+++ src/components/molecules/menu/IntroMenu.jsx
@@ -368,23 +368,20 @@ export default function IntroMenu() {
                   </div>
                 </div>
                 {count == 0 && (
-                  <div
+                  <button
                     onClick={() => {
                       setIntro(false);
                     }}
+                    className="block px-5 py-3 mx-auto mt-3 text-xl orbitron font-[600] text-black transition-all duration-500 border-2 border-black cursor-pointer bg-amber-400 rounded-xl w-fit hover:text-amber-400 hover:bg-black hover:border-amber-400"
                   >
-                    <Heading
+                    <Paragraph
                       language={language}
-                      className={
-                        "text-white font-semibold drop-shadow-[0_0_5px_#fff] hover:scale-125 transition-all ease-in-out cursor-pointer text-center"
-                      }
-                      level={1}
                       text={{
                         en: "Start",
                         es: "Continuar",
                       }}
-                    ></Heading>
-                  </div>
+                    ></Paragraph>
+                  </button>
                 )}
                 {count >= 1 && (
                   <div>
